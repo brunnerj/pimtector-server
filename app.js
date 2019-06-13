5b4a2c66f3e76c8f6fa03adb9050e57b35ffdf21
James Brunner <james@brunnertechnicalservices.com>
2019-06-12 23:22:56 -0600

    Start creating the actual app and start with a battery service.

---
+++ app.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const SERVER_APP_NAME = 'PIMtector';
+
+const bleno = require('bleno');
+
+
+// Set up the service classes...
+
+// Battery Service
+// Read only battery level between 0 and 100 percent
+const BatteryService = new require('./battery-service')();
+
+
+// Group services and uuid's for easy consumption
+const Services = [ BatteryService ];
+const ServiceUUIDs = [ BatteryService.uuid ];
+
+
+console.log(`${SERVER_APP_NAME} starting bleno server...`);
+
+
+// Wait for power on to start advertising our services
+bleno.on('stateChange', state => {
+
+	console.log(`[bleno] Adapter changed state to ${state}`);
+
+	if (state === 'poweredOn') {
+		
+		bleno.startAdvertising(SERVER_APP_NAME, ServiceUUIDs, err => {
+			if (err) console.error(err);
+		});
+
+	} else {
+		console.log(`Stopping advertising since state is ${state} (instead of poweredOn).`);
+		bleno.stopAdvertising();
+	}
+});
+
+
+// Configure the services when advertising starts
+bleno.on('advertisingStart', err => {
+	console.log('[bleno] advertisingStart')
+
+	if(err) {
+		console.error(err);
+		return;
+	}
+
+	console.log('Configuring services...');
+	
+	bleno.setServices(Services, err => {
+		if(err)
+			console.error(err);
+		else
+			console.log('Services configured');
+	});
+});
+
+// diagnostic messages
+bleno.on('advertisingStartError', err => console.error('[bleno] advertisingStartError'));
+bleno.on('advertisingStop', err => console.log('[bleno] advertisingStop'));
+
+bleno.on('servicesSet', err => console.log('[bleno] servicesSet'));
+bleno.on('servicesSetError', err => console.log('[bleno] servicesSetError'));
+
+
+// Notify the console that we've accepted a connection
+// and stop advertising
+bleno.on('accept', function(clientAddress) {
+	console.log(`[bleno] accept ${clientAddress}`);
+	bleno.stopAdvertising();
+});
+
+
+// Notify the console that we have disconnected from a client
+// and start advertising
+bleno.on('disconnect', function(clientAddress) {
+	console.log(`[bleno] disconnect ${clientAddress}`);
+	bleno.startAdvertising();
+});
---
+++ battery-service.js
@@ -0,0 +1,48 @@
+const bleno = require('bleno');
+
+class BatteryLevelCharacteristic extends bleno.Characteristic {
+	constructor() {
+		super({
+			uuid: '2A19',
+			properties: ['read'],
+			descriptors: [
+				new bleno.Descriptor({
+					uuid: '2901',
+					value: 'Battery level between 0 and 100 percent'
+				}),
+				new bleno.Descriptor({
+					uuid: '2904',
+					value: new Buffer([0x04, 0x01, 0x27, 0xAD, 0x01, 0x00, 0x00 ])
+				})
+			]
+		});
+
+		this.name = 'Battery';
+	}
+
+	onReadRequest(offset, callback) {
+		try {
+			const fakeLevel = Math.floor(Math.random()*(100-30+1)+30); // fake level between 30 and 100 (%)
+			const result = new Buffer([ fakeLevel ]);
+
+			console.log(`Returning battery result: ${result}`);
+
+			callback(this.RESULT_SUCCESS, data);
+
+		} catch (err) {
+
+			console.error(err);
+			callback(this.RESULT_UNLIKELY_ERROR);
+		}
+	}
+}
+
+
+const BatteryService = new bleno.PrimaryService({
+	uuid: '180F',
+	characteristics: [
+		new BatteryLevelCharacteristic()
+	]
+});
+
+module.exports = BatteryService;
